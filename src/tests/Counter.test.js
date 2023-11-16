e9e7f4e5455a07ed2892a4088ccf7de4a2ad12d8
Lauren Escarcha <laurenescarcha@ufl.edu>
2023-11-16 17:10:47 -0500

    added passed tests

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,36 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import {render, fireEvent, screen} from '@testing-library/react'
+import Counter from '../components/Counter';
 
 beforeEach(() => {
   // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const countMessage = screen.getByText("Counter");
+  expect(countMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  const count = screen.getByTestId("count").textContent;
+  expect(count).toBe("0")
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const count = screen.getByTestId("count");
+  const increment = screen.getByText("+");
+
+  fireEvent.click(increment);
+
+  expect(count.textContent).toBe("1");
+  
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const count = screen.getByTestId("count");
+  const decrement = screen.getByText("-");
+
+  fireEvent.click(decrement);
+
+  expect(count.textContent).toBe("-1");
 });
